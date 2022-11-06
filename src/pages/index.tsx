bfc042d021b1ec93c781a60a3213fce92df714cb
faithandjs <okogbofaith@email.com>
2022-11-06 21:36:10 +0100

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,26 @@
+import Card from "../components/card";
+import { dataProp } from "../types/type";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const data: dataProp[] = require(`../data/data.json`);
+  const [active, setActive] = useState(0);
+  useEffect(() => {
+    console.log(active);
+  }, [active]);
+
+  return (
+    <div className='h-[100vh] flex flex-col justify-end'>
+      <ul className='h-[50vh] flex items-end mb-40 '>
+        {data.map((item, key) => {
+          const getProps = {
+            item,
+            setActive,
+            active,
+          };
+          return <Card key={key} {...getProps} />;
+        })}{" "}
+      </ul>
+    </div>
+  );
+}
